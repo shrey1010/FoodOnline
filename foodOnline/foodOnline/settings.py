f5ea818e8dfac9e8bc76408647482bce68e56b34
Shrey <shreyshukla1010@gmail.com>
2023-08-27 11:36:06 +0530

    intigrating postgresql

--- foodOnline/foodOnline/settings.py
+++ foodOnline/foodOnline/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "foodOnline.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "FoodOnline_db",
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
     }
 }
 
