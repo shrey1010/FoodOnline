aa6de4b0268c896376b414ba3de05478fd205757
Shrey <shreyshukla1010@gmail.com>
2023-08-27 12:59:56 +0530

    creating custom user model

---
+++ foodOnline/accounts/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self,first_name,last_name,username,email ,  password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have an username")
+            
+        if not password:
+            raise ValueError("Users must have a password")
+        
+        user_obj  = self.model(
+            email = self.normalize_email(email),
+            username= username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user_obj.set_password(password)
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_superuser(self, first_name, last_name, username, email,  password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            username = username,
+            last_name=last_name,
+            password = password ,
+        )
+        user.is_superadmin = True
+        user.is_staff = True
+        user.is_admin = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user 
+
+
+class User(AbstractBaseUser):
+    RESTAURANT = 1
+    CUSTOMER = 2
+    ROLE_CHOICE = (
+        (RESTAURANT,'Restaurant'),
+        (CUSTOMER,'Customer'),
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12,blank=True)
+    role =  models.PositiveSmallIntegerField(choices=ROLE_CHOICE,blank=True,null=True)
+
+    #required fields
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','first_name','last_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+
+    def has_module_perms(self, app_label):
+        return True
+    
+
--- foodOnline/foodOnline/settings.py
+++ foodOnline/foodOnline/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -83,6 +84,8 @@ DATABASES = {
         "HOST": config("DATABASE_HOST"),
     }
 }
+
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
