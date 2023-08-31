8f247bcd89839b9d5d8d8706c845cc817af03af3
Shrey <shreyshukla1010@gmail.com>
2023-09-01 00:21:17 +0530

    configuring validations django messages

--- foodOnline/accounts/forms.py
+++ foodOnline/accounts/forms.py
@@ -6,4 +6,13 @@ class UserForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password','confirm_password']
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match with confirm password")
+        return cleaned_data
--- foodOnline/accounts/views.py
+++ foodOnline/accounts/views.py
@@ -1,27 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 # Create your views here.
 
 def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name,username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
-            return HttpResponse('User Created')
+            messages.success(request, 'Your account has registered successfully!')
+            return redirect('registerUser')
+        else :
+            print("Invalid Form")
     else:
         form = UserForm()
-        context = { 
-            'form ': form,
+    context = { 
+            'form': form,
         }
-        return render(request,'accounts/registerUser.html',context = context )
-        form = UserForm 
-        context = {
-            'form ': form,
-        }
-        return render(request,'accounts/registerUser.html',context = context )
+    return render(request,'accounts/registerUser.html',context = context )
 
 
