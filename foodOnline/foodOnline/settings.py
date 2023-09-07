67e3771ddd96d35561ff176b232698a442e428c7
Shrey <shreyshukla1010@gmail.com>
2023-09-08 00:28:17 +0530

    google autocomplete via googlegeocoding

--- foodOnline/foodOnline/settings.py
+++ foodOnline/foodOnline/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "accounts.context_processors.get_vendor",
+                "accounts.context_processors.get_google_api",
             ],
         },
     },
@@ -151,3 +152,7 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+
+# Google api configuration
+GOOGLE_API_KEY = config('GOOGLE_API_KEY')
