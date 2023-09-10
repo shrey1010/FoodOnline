1eb79989bed40de1ac62287ffd9ee86adfc4c672
Shrey <shreyshukla1010@gmail.com>
2023-09-10 23:05:44 +0530

    showing food page in marketplace

--- foodOnline/marketplace/urls.py
+++ foodOnline/marketplace/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views 
 
 urlpatterns = [
-    path('marketplace/',views.marketplace,name = "marketplace")
-
+    path('marketplace/',views.marketplace,name = "marketplace"),
+    path('<slug:vendor_slug>/',views.vendor_detail,name = 'vendor_detail'),
+    # path('vendor_list/',views.vendor_list,name = 'vendor_list'),
 ]
--- foodOnline/marketplace/views.py
+++ foodOnline/marketplace/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from vendor.models import Vendor
+from django.shortcuts import get_object_or_404
+from menu.models import Category,FoodItem
+from django.db.models import Prefetch
 # Create your views here.
 
 def marketplace(request):
@@ -10,3 +13,20 @@ def marketplace(request):
         'vendor_count': vendor_count,
     }
     return render(request, 'marketplace/listings.html',context=context)
+
+
+def vendor_detail(request,vendor_slug):
+    vendor = get_object_or_404(Vendor,vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset= FoodItem.objects.filter(is_available=True)
+        )
+    )
+    # vendor = Vendor.objects.get(vendor_slug=vendor_slug)
+    context = {
+        'vendor': vendor,
+        'categories': categories,
+       
+    }
+    return render(request, 'marketplace/vendor_detail.html',context=context)
